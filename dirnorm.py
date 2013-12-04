14493278f79852d2e2b670167ce8f661dba807af
Victor Xiangdong Huang <bayvictor@gmail.com>
2013-12-04 07:11:38 -0800

    first commit

--- dirnorm.py
+++ dirnorm.py
@@ -22,6 +22,11 @@ def replace_illegal_char_in_filename():
     new_line = ''.join( ch if ch.isalnum() else '_' for ch in line )
     words = new_line.split("_")
     new_line2 = '_'.join( word for word in words[:-1] ) + "." + words[-1]
+    for kk in range(0,len(new_line2)):
+      if new_line2[kk] != '_':
+        break
+
+    new_line2 = new_line2[kk:]
     os.system("mv "+ "\"" + line+"\" "+ new_line2 + " " )
 
 if __name__=="__main__":
